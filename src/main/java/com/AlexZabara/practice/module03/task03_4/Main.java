fdb0670b122bd00d1c574b53307051677b7249e6
zabara <zabara_a@mail.ru>
2017-02-17 19:23:40 +0200

    new

---
+++ src/main/java/com/AlexZabara/practice/module03/task03_4/Main.java
@@ -0,0 +1,10 @@
+package main.java.com.AlexZabara.practice.module03.task03_4;
+
+public class Main {
+    public static void main(String[] args) {
+        Main main = new Main();
+        
+    }
+
+
+}
--- src/main/java/com/AlexZabara/practice/module03/task03_4/User.java
+++ src/main/java/com/AlexZabara/practice/module03/task03_4/User.java
@@ -1,4 +1,77 @@
 package main.java.com.AlexZabara.practice.module03.task03_4;
 
 public class User {
+    String name;
+    int balance;
+    int monthsOfEmployment;
+    String companyName;
+    int salary;
+    String currency;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public int getMonthsOfEmployment() {
+        return monthsOfEmployment;
+    }
+
+    public void setMonthsOfEmployment(int monthsOfEmployment) {
+        this.monthsOfEmployment = monthsOfEmployment;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+    public User(String name, int balance, int monthsOfEmployment, String companyName, int salary, String currency) {
+        this.name = name;
+        this.balance = balance;
+        this.monthsOfEmployment = monthsOfEmployment;
+        this.companyName = companyName;
+        this.salary = salary;
+        this.currency = currency;
+    }
+
+    void paySalary(){
+        System.out.println("Ваш баланс = " + getName() + getBalance());
+        System.out.println("пополняем = " + getSalary());
+        balance += salary;
+        System.out.println("Итог = " + getBalance());
+    }
+    void withdraw(int sum){
+
+    }
+
 }
